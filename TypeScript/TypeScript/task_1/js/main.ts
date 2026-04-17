683f7e663bf70117929acbbe1977832fd5a037bf
root <root@ip-10-42-135-187.ec2.internal>
2026-04-17 13:54:09 +0000

    Add StudentClass with interfaces

--- TypeScript/TypeScript/task_1/js/main.ts
+++ TypeScript/TypeScript/task_1/js/main.ts
@@ -32,3 +32,42 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
 };
 
 console.log(printTeacher("John", "Doe"));
+
+
+/* ===== Task 4 ===== */
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+/* test */
+
+const student = new StudentClass('John', 'Doe');
+
+console.log(student.displayName()); // John
+console.log(student.workOnHomework()); // Currently working
